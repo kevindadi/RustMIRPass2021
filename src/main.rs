bade83706812cecc6682a3dbedbe9b912febe905
kevindadi <786809627@qq.com>
2022-11-09 00:10:29 +0800

    rustmirpass2021

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 #![feature(rustc_private)]
 
 extern crate env_logger;
-extern crate getopts;
+// extern crate getopts;
 #[macro_use]
 extern crate log;
 // extern crate log_settings;
-extern crate rustc;
-extern crate rustc_metadata;
 extern crate rustc_driver;
 extern crate rustc_errors;
-extern crate rustc_codegen_utils;
 extern crate rustc_interface;
-extern crate syntax;
+extern crate rustc_middle;
+extern crate rustc_session;
+extern crate rustc_hir;
 
 use rustc_interface::{interface, Queries};
-use rustc::hir::def_id::LOCAL_CRATE;
+use rustc_hir::def_id::LOCAL_CRATE;
 use rustc_driver::Compilation;
-use rustc::mir;
-use rustc::mir::traversal;
+use rustc_middle::mir;
+use rustc_middle::mir::traversal;
 
 struct HelloCalls {
     data: i32,
+}
+
+impl HelloCalls {
+    fn new() -> Self{
+        Self {
+            data: 1i32
+        }
+    }
 }
 
 impl rustc_driver::Callbacks for HelloCalls {
@@ -33,8 +40,8 @@ impl rustc_driver::Callbacks for HelloCalls {
         compiler.session().abort_if_errors();
 
         queries.global_ctxt().unwrap().peek_mut().enter(|tcx| {
-            let (entry_def_id, _) = tcx.entry_fn(LOCAL_CRATE).expect("no main function found!");
-            println!(entry_def_id);
+            let (entry_def_id, _) = tcx.entry_fn(()).expect("no main function found!");
+            println!("{:?}", entry_def_id);
         });
         Compilation::Stop
     }
@@ -53,12 +60,31 @@ fn main() {
     for arg in std::env::args() {
         rustc_args.push(arg);
     }
+    let sysroot: String = "--sysroot".into();
+    rustc_args.push(sysroot);
+    rustc_args.push(find_sysroot());
     println!("rustc args: {:?}", rustc_args);
     rustc_driver::install_ice_hook();
     let result = rustc_driver::catch_fatal_errors(move || {
         println!("Enter Callback");
-        rustc_driver::run_compiler(&rustc_args, &mut HelloCalls {data: 1}, None, None)
+        let mut helloCalls = HelloCalls::new();
+        let compiler = rustc_driver::RunCompiler::new(&rustc_args, &mut helloCalls);
+        compiler.run()
     }).and_then(|result| result);
     println!("After Callback: {}", result.is_err() as i32);
     std::process::exit(result.is_err() as i32);
+}
+
+fn find_sysroot() -> String {
+    let home = option_env!("RUSTUP_HOME");
+    let toolchain = option_env!("RUSTUP_TOOLCHAIN");
+    match (home, toolchain) {
+        (Some(home), Some(toolchain)) => format!("{}/toolchains/{}", home, toolchain),
+        _ => option_env!("RUST_SYSROOT")
+            .expect(
+                "Could not find sysroot. Specify the RUST_SYSROOT environment variable, \
+                 or use rustup to set the compiler to use for LOCKBUD",
+            )
+            .to_owned(),
+    }
 }
